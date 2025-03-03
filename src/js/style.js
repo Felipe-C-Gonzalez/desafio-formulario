b971b5c1320e0b38250b62526ffdcec9c946464b
Felipe Gonzalez <felipecgonzalez2305@gmail.com>
2025-03-03 19:08:03 -0300

    Código refatorado, e foi incluso a responsividade

--- src/js/style.js
+++ src/js/style.js
@@ -1,25 +1,23 @@
-const inputs = document.querySelectorAll('.input-text');
+const formField = document.querySelectorAll('.input-text');
 const submitButton = document.querySelector('.btn-toSend');
 
-submitButton.addEventListener('click', function (event) {
-    let formIsValid = true;
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
 
-    inputs.forEach(function (input) {
+    formField.forEach((input) => {
         const errorMessage = input.nextElementSibling;
 
-        if (input.value === '') {
-            input.classList.add('without-fill');
-            errorMessage.style.display = 'block';
-            formIsValid = false;
-        } else {
+        if (input.value !== '') {
             input.classList.remove('without-fill');
-            input.classList.add('filled')
-            errorMessage.style.display = 'none';
+            input.classList.add('filled');
+            errorMessage.classList.remove('show')
+
+
+        } else {
+            input.classList.remove('filled');
+            input.classList.add('without-fill');
+            errorMessage.classList.add('show');
         }
     })
-
-    if (!formIsValid) {
-        event.preventDefault();
-    }
 })
 
